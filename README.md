68fe1284d8f4099993e1657dae6873a3ee9646c1
Drew Harris <contac@grmnygrmny.com>
2012-11-08 13:04:41 -0500

    Big change - now require weatherman and pass in a config.  See readme

--- README.md
+++ README.md
@@ -4,23 +4,60 @@ CloudWatch metrics client.  Generates metrics and submits them as custom metrics
 
 ## Install
 
-     npm install node-weatherman
+     npm install weatherman
 
 ## Running
 
-    git clone
-    npm install
-    ./weatherman
+Include weatherman, assign a config object to it and pass an interval in seconds to weatherman.start().
+
+	var wm = require('weatherman');
+
+	wm.config = {
+		namespace = "SW/ServiceName"
+		plugins: {
+	    	plugin_name: {
+	    		config_key: "config_value"
+	    	}
+		}
+	};
+
+	wm.start(60);
+
+## Stopping
+
+	wm.stop();
+
 
 ## Configuration
 
-Configuration is done via nconf.  Copy config.json.example to config.json and edit.  Add plugins to the 'active_plugins' field to enable them.
+If a namespace is not defined in the plugin-specific configuration, the globally-defined namespace is used.
 
-### Plugins
+### system
 
-* system: load, free memory and uptime stats
-* rabbit: Rabbit queue statistics - metadata, phantom and celery
-* listening: current number of listeners on exfm
-* nurse: node-nurse integration, system info and server statistics
+Load, free memory and uptime stats
 
+* No configuration
+
+### rabbit
+
+Rabbit queue statistics - metadata, phantom and celery.
+
+* rabbit_url: the Rabbit queue API URL to connect to
+* rabbit_auth: username and password for Rabbit access in a list
+
+### listening
+
+Current exfm listening statistics.
+
+* listening_url: listening URL to grab stats from
+
+### nurse
+
+[Nurse](https://github.com/exfm/node-nurse) integration - system info and server statistics.
+
+* nurse_url: URL to grab info from, usually localhost:port/health-check
+* nurse_stats: list of statistics to grab, see nurse docs
+* nurse\_mem\_stats: list of memory statistics to grab, see nurse docs
+* nurse\_load\_stats: list of load statistics to grab, see nurse docs
+* nurse\_server\_stats: list of server statistics to grab, see nurse docs
 
---
+++ index.js
@@ -0,0 +1,86 @@
+"use strict";
+
+var sequence = require('sequence'),
+	when = require('when'),
+	fs = require('fs'),
+	os = require('os'),
+	nconf = require('nconf'),
+	getConfig = require('junto'),
+	aws = require('plata');
+
+var availablePlugins = {},
+	juntoConfig,
+	timer;
+
+fs.readdirSync(__dirname + '/plugins').forEach(function(pluginFilename){
+	availablePlugins[pluginFilename.split('.')[0]] = require(__dirname + '/plugins/' + pluginFilename);
+});
+
+getConfig('development').then(function(c){
+	juntoConfig = c;
+	aws.connect(juntoConfig.aws);
+});
+
+module.exports.config = {};
+
+module.exports.start = function(ivl){
+	console.log('starting weatherman...');
+	generateMetrics();
+	timer = setInterval(function(){
+		generateMetrics();
+	}, ivl*1000);
+};
+
+module.exports.stop = function(){
+	console.log('stopping weatherman...');
+	clearInterval(timer);
+};
+
+function generateMetrics(){
+	var allMetrics = {},
+		timestamp = new Date().toISOString(),
+		pluginOpts;
+
+	when.all(Object.keys(module.exports.config.plugins).map(function(plugin){
+		var p = when.defer();
+		pluginOpts = module.exports.config.plugins[plugin];
+		availablePlugins[plugin].addMetrics(timestamp, pluginOpts).then(function(data){
+			if (pluginOpts !== undefined && pluginOpts.namespace !== undefined) {
+				allMetrics[pluginOpts.namespace] = data;
+				return p.resolve();
+			}
+			else {
+				allMetrics[module.exports.config.namespace] = data;
+				return p.resolve();
+			}
+		});
+		return p.promise;
+	})).then(function(){
+		sendMetricsToCloudWatch(allMetrics).then(function(resp){
+			console.log('done');
+		});
+	});
+}
+
+function sendMetricsToCloudWatch(metrics){
+	var d = when.defer();
+	sequence().then(function(next){
+		aws.onConnected(next);
+	}).then(function(next){
+		when.all(Object.keys(metrics).map(function(namespace){
+			var p = when.defer();
+			// aws.cloudWatch.putMetricData(namespace, metrics[namespace]).then(function(){
+			//     return p.resolve();
+			// });
+			console.log('ns:' + module.exports.config.namespace);
+			console.log(metrics[namespace]);
+			p.resolve();
+			return p.promise;
+		})).then(function(){
+			next();
+		});
+	}).then(function(next){
+		d.resolve();
+	});
+	return d.promise;
+}
